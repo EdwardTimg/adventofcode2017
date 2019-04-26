a6133bf6fe8ffd0ee9026a90878094f057bae997
EdwardTimg <edward@timgren.se>
2019-04-26 12:34:02 +0200

    TDD approch for day 4 part 2

--- src/main/java/DayFourPartTwo.java
+++ src/main/java/DayFourPartTwo.java
@@ -1,9 +1,29 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DayFourPartTwo {
 
-    //here will be some code
-    // here will there be more code
+    public static void main(String[] args) {
+        DayFourPartTwo dayFourPartTwo = new DayFourPartTwo();
+        try{
+            int nrValidPasscodes = 0;
+            BufferedReader reader = new BufferedReader(new FileReader("inputdayfour.txt"));
+            String line;
+            while((line = reader.readLine())!=null){
+                if(dayFourPartTwo.checkValidPasscode(line)){
+                    nrValidPasscodes ++;
+                }
+            }
+            System.out.println(nrValidPasscodes);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 
     public boolean anagramCompare(String first, String second)
@@ -15,7 +35,19 @@ public class DayFourPartTwo {
             char[] secondArray = second.toLowerCase().toCharArray();
             Arrays.sort(firstArray);
             Arrays.sort(secondArray);
-            return firstArray.equals(secondArray);
+            return Arrays.equals(firstArray, secondArray);
+            //return firstArray.equals(secondArray);
         }
+    }
+    public boolean checkValidPasscode(String line){
+        String[] values = line.split("\\s+");
+        for (int i = 0; i <values.length ; i++) {
+            for (int j = 0; j <values.length ; j++) {
+                    if(i != j && anagramCompare(values[i], values[j])){
+                        return false;
+                    }
+            }
+        }
+        return  true;
     }
 }
--- src/test/java/DayFourPartTwoTests.java
+++ src/test/java/DayFourPartTwoTests.java
@@ -1,15 +1,35 @@
+
+
+
 import org.junit.Test;
 
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 
 public class DayFourPartTwoTests {
+    DayFourPartTwo dayFourPartTwo = new DayFourPartTwo();
     @Test
-    public void anagramCompare(){
-        DayFourPartTwo dayFourPartTwo = new DayFourPartTwo();
+    public void anagramCompare_NoAnagram(){
+
         assertFalse( dayFourPartTwo.anagramCompare("abcde","fghij"));
-
-
     }
+    @Test
+    public void anagramCompare_Anagram(){
+
+        assertTrue( dayFourPartTwo.anagramCompare("aa","aa"));
+    }
+
+    @Test
+    public void checkPasscode_Valid(){
+        assertTrue(dayFourPartTwo.checkValidPasscode( "aa bb cc dd ee"));
+    }
+    @Test
+    public void checkPasscode_inValid(){
+        assertFalse(dayFourPartTwo.checkValidPasscode( "aa bb cc dd aa"));
+    }
+
+
+
 }
 
